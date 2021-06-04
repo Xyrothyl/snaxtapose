c64937ce85101ffac66cd7650969059c25d1f573
Chris <chris.m.carbone@gmail.com>
2021-06-03 23:01:56 -0700

    added basic command framework and added manual food input option

--- food.py
+++ food.py
@@ -1,8 +1,10 @@
+# food_nutrient.csv has amounts per 100g of food
+
 class Food:
 
     def __init__(self, lst):
         self.name = lst[0]
-        self.cost = int(lst[1])
+        self.cost = float(lst[1])
         self.amnt = int(lst[2])
         self.unit = lst[3]
         self.cals = int(lst[4])
@@ -10,10 +12,14 @@ class Food:
         self.carb = int(lst[6])
         self.prot = int(lst[7])
         self.upc = lst[8]
-        self.metric = -1
+        self.metric = -1  # Should always be set back to -1 after use
 
     def __lt__(self, other):
         return self.metric < other.metric
 
     def __str__(self):
         return self.name
+
+    def printInfo(self):
+        print("{}: ${:.2f} for {} {}".format(self.name, self.cost, self.amnt, self.unit))
+        print("{} calories, {} g fat, {} g carbs, {} g protein".format(self.cals, self.fats, self. carb, self.prot))
--- snaxtapose.py
+++ snaxtapose.py
@@ -10,8 +10,9 @@ def openPantry():
             pantry.append(Food(item))
             cnt += 1
 
-    for entry in pantry:
-        print(entry)
+    # for entry in pantry:
+    #     print(entry)
+
     return cnt
 
 
@@ -29,21 +30,66 @@ def updatePantry():
     print("Pantry updated.")
 
 
-def shop(shopping_list):
+def shop():
     cart = []
-    with open("branded_food.csv") as store_f:
-        for line in store_f:
-            x = 1  # filler
+    # TODO: SQL database for this
+    # with open("branded_food.csv") as store_f:
+    #     for line in store_f:
+    #         x = 1  # temp filler
+
+    print("Shopping. Enter 'checkout' to exit.")
+
+    while True:
+        s = input("Food name: ").lower()
+        if s == "checkout":
+            break
+
+        f = [s, -1, -1, -1, -1, -1, -1, -1, -1]
+
+        f[1] = input("Price: ")
+        f[3] = input("Amount unit: ")
+        f[2] = input("Amount: ")
+        f[4] = input("Calories: ")
+        f[5] = input("Fat: ")
+        f[6] = input("Carbohydrates: ")
+        f[7] = input("Protein: ")
+        '''
+        food[8] = input("UPC: ")
+        '''
+        # TODO: Ask for confirmation of data & correct mistakes
+        food = Food(f)
+        food.printInfo()
+        print("Adding food to cart...", end='')
+        cart.append(food)
+        print(" added!")
+        print("_\\|/_ _\\|/_ _\\|/_ _\\|/_ ")
 
     return cart
+
+
+# TODO
+def setMetric():
+    return 0
+
+
+# TODO
+def compare():
+    return 0
 
 
 pantry = []
 pantry_cnt = openPantry()
 
+# TODO: Full list of commands w/ functionality
+while True:
+    command = input("What would you like to do? ").lower()
+    if command == "exit" or command == "quit":
+        break
+    elif command == "shop":
+        shop()
+
+# This might want to go in the while loop, idk yet
 if len(pantry) > pantry_cnt:
     updatePantry()
 
 # TESTING
-
-
